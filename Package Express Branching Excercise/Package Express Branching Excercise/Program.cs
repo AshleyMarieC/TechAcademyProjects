c970bbff141700852223715e2c301a886a569694
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-18 17:16:43 -0700

    Add files via upload
    Branching Assignment

---
+++ Package Express Branching Excercise/Package Express Branching Excercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace Package_Express_Branching_Excercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below \nPlease enter your package weight:");
+            string enterWeight = Console.ReadLine();
+            double weight = Convert.ToDouble(enterWeight);
+            if (weight > 50)
+            {
+                Console.WriteLine("ERROR: Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                string enterWidth = Console.ReadLine();
+                Console.WriteLine("Please enter the package height:");
+                string enterHeight = Console.ReadLine();
+                Console.WriteLine("Please enter the package length:");
+                string enterLength = Console.ReadLine();
+
+                double width = Convert.ToDouble(enterWidth);
+                double height = Convert.ToDouble(enterHeight);
+                double length = Convert.ToDouble(enterLength);
+
+                if (width + height + length > 50)
+                {
+                 Console.WriteLine("ERROR: Package too big to be shipped via Package Express.");
+                 Console.ReadLine();
+                }
+                else
+                {
+                    double total = width + height + length * 100;
+                    string quote = string.Format("{0:c}", Convert.ToDecimal(total));
+                    Console.WriteLine("Your estimated total for shipping this package is:"+quote);
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
