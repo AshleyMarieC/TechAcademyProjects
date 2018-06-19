3c6cf1b077dd4ac720fc30085194bdd7decbad03
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-19 11:20:57 -0700

    Add files via upload
    Strings Assignment

---
+++ Strings Assignment/Strings Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+
+namespace Strings_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Concatenate three strings
+            string scoops = "two";
+            string cone = "waffle";
+            string flavor = "chocolate";
+            Console.WriteLine("You have ordered " + scoops + " scoops of " + flavor + " ice cream on a " + cone + " cone!");
+            Console.ReadLine();
+
+            //Convert a string to uppercase.
+            string name = "ashley marie";
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            //Create a Stringbuilder and build a paragraph of text, one sentence at a time.
+            StringBuilder newString = new StringBuilder();
+            newString.Append("Down they go!");
+            newString.AppendLine();
+            newString.Append("Hail and Snow!");
+            newString.AppendLine();
+            newString.Append("Freezes and Sneezes and Noses will blow!");
+            newString.AppendLine();
+            newString.Append("-Rohl Dahl");
+            Console.WriteLine(newString);
+            Console.ReadLine();
+
+
+        }
+    }
+}
