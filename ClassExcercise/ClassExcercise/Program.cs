ffb02fe0b10551826ccc227625924b59283376e4
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-25 16:29:21 -0700

    Add files via upload
    1. Create a class. In that class, create a method that will take in an integer, do a math operation to it and then 
       return the answer as an integer.
    
    2. In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. 3. Display the result to the screen.
    
    3. Add a second method to the class, with the same name, that will take in a decimal, do a different math 
       operation to it and then return the answer as an integer.
    
    4. In the Main() method of the console app, instantiate the class and call the second method, passing in a 
       decimal. Display the result to the screen.
    
    5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if 
       possible, do a different math operation to it and then return the answer as an integer.
    
    6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string 
       that equates to an integer. Display the result to the screen.

---
+++ ClassExcercise/ClassExcercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOp operation = new mathOp();
+            Console.WriteLine("Three multiplied by 6  = " + operation.Square(3,6).ToString());
+            Console.ReadLine();
+
+         
+            Console.WriteLine("11.653 + 11.653 = " + operation.Add(Convert.ToInt32(11.653)).ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("Hawaii containes this many letters = " + operation.length("Hawaii").ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ClassExcercise/ClassExcercise/mathOp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExcercise
+{
+    public class mathOp
+    {
+        public int Square(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public decimal Add(decimal num1)
+        {
+            return num1 + num1;
+        }
+        public string length(string string1)
+        {
+            return Convert.ToString(string1.Length);
+        }
+    }
+}
