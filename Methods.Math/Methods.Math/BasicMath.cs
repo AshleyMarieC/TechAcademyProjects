ed92603fd49c769acfef48bb2180539aa33d9f93
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-25 13:13:45 -0700

    Add files via upload
    Learning about methods and classes.
    
    1. Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some math operation on the received parameter. Put this class in a separate .cs file in the application.
    2. In the Main() program, ask the user what number they want to do the math operations on.
    3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.

---
+++ Methods.Math/Methods.Math/BasicMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods.Math
+{
+    public class BasicMath
+    {
+            public int Square(int num1)
+            {
+                return num1 * num1;
+            }
+            public int Add(int num1)
+            {
+                return num1 + num1;
+            }
+            public int Subtract(int num1)
+            {
+            return num1 - num1;
+            }
+    }
+    
+}
+
+
+
---
+++ Methods.Math/Methods.Math/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods.Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do the math operations on?: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            BasicMath sq = new BasicMath();
+            Console.WriteLine(num1 + " x " + num1 + " = " + sq.Square(num1).ToString());
+            Console.WriteLine(num1 + " + " + num1 + " = " + sq.Add(num1).ToString());
+            Console.WriteLine(num1 + " - " + num1 + " = " + sq.Subtract(num1).ToString());
+            Console.ReadLine();
+        }
+
+    
+    }
+}
