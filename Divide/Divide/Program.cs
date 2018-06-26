da89eb7ef8b1723885fce3bdaf8bdfc5415f9d55
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-26 16:45:16 -0700

    Add files via upload
    1. Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
    
    2. In the Main() method, instantiate that class.
    
    3. Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two. 
    
    4. Create a method with output parameters.
    
    5. Overload a method.
    
    6. Declare a class to be static.

---
+++ Divide/Divide/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result1; //pass variable instead of value to void method
+
+            Console.WriteLine("Please Enter A Number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            divMethod.Method(number1, out result1);
+            Console.WriteLine("Result is: " + result1);
+
+            Console.ReadLine();
+
+
+
+            outMethod Method2 = new outMethod();
+            Method2.OutExample(out int number);
+            Console.WriteLine(number);
+            Console.ReadLine();
+
+
+            Console.WriteLine(classStatic.MyStruct.num4);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Divide/Divide/divMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide
+{
+    public class divMethod
+    {
+        public static void Method(int number1, out int result1)
+        {
+            result1 = number1 / 2;
+        }
+    }
+
+    public class outMethod //Declare a class to be static.
+    {
+        public void OutExample(out int number) //Create a method with output parameters.
+        {
+            number = 125;
+        }
+        public void OutExample(out string number) //Overload a method.
+        {
+            number = "Fourty Four";
+        }
+    }
+
+    static public class classStatic
+    {
+        public struct MyStruct
+        {
+            public static int num4 = 4;
+        }
+           
+    }
+
+}
