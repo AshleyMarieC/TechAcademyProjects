3ad51297d37d0e1647993a56ff86ecf266ab04d5
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-26 10:53:25 -0700

    Update Program.cs
    Added second exercise
    
    1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them 
       optional. Have the method do a math operation and return a integer result.
    
    2. In the Main() method of the console app, instantiate the class.
    
    3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second 
       number.
    
    4. Call the method in the class, passing in the one or two numbers entered.

--- ClassExcercise/ClassExcercise/Program.cs
+++ ClassExcercise/ClassExcercise/Program.cs
@@ -20,6 +20,26 @@ namespace ClassExcercise
 
             Console.WriteLine("Hawaii containes this many letters = " + operation.length("Hawaii").ToString());
             Console.ReadLine();
+            
+            //Added new exercise 
+            
+            class2 operation2 = new class2();
+
+            Console.WriteLine("Please Enter A Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Another Number:");
+            string num2 = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(num2))
+            {
+                Console.WriteLine(operation2.multiply(num1, num2: 5).ToString()); Console.ReadLine();
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                Console.WriteLine(operation2.multiply(num1, num3).ToString());
+            }
+            Console.ReadLine();
 
         }
     }
