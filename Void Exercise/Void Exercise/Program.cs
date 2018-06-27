e51131a89e02cebba3d8c44ff5a600edcfc72f36
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-26 17:09:38 -0700

    Add files via upload
    1. Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a 
       math operation on the first integer and display the second integer to the screen. 
    
    2. In the Main() method of the console app, instantiate the class.
    
    3. Call the method in the class, passing in two numbers. 
    
    4. Call the method in the class, specifying the parameters by name.

---
+++ Void Exercise/Void Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result3; //pass variable instead of value to void method
+            int result2;
+
+            int number4 = 50;
+            int number5 = 10;
+
+            voidClass.Method(number4, number5, out result3, out result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Void Exercise/Void Exercise/voidClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Exercise
+{
+    public class voidClass
+    {
+        public static void Method(int number4, int number2, out int result3, out int result2)
+        {
+            result3 = number4 * 10;
+            result2 = number2;
+        }
+    }
+}
