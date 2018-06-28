2e9f3705e7d04631a49b805a7ca1f528cd98c62e
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-28 10:34:43 -0700

    Update Program.cs
    updated

--- Void Exercise/Void Exercise/Program.cs
+++ Void Exercise/Void Exercise/Program.cs
@@ -11,17 +11,13 @@ namespace Void_Exercise
         static void Main(string[] args)
         {
 
-            int result3; //pass variable instead of value to void method
-            int result2;
+            voidClass VoidClass = new voidClass();
 
-            int number4 = 50;
-            int number5 = 10;
+            VoidClass.Method(number4:16, number2: 10);
 
-            voidClass.Method(number4, number5, out result3, out result2);
-            Console.WriteLine(result3);
-            Console.WriteLine(result2);
-
+            Console.WriteLine(voidClass.result);
             Console.ReadLine();
         }
     }
 }
+
