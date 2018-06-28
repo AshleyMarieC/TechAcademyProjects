ba47aa0e12b0758ab2b8b15641ff5604f090bdb9
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-28 14:38:41 -0700

    Add files via upload

---
+++ Assignment Pg69/Assignment Pg69/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Pg69
+{
+    public class Person //Base Class
+    {
+        public string FirstName;
+        public string LastName;
+
+    public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName +" "+ LastName);
+        }
+    }
+
+    public class Employee : Person //Derived Class : Base Class
+    {
+        int ID;
+    }
+}
