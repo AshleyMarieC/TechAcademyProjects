2072ddcb1992dab8c83a12996a0892be81958243
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-06-28 19:42:06 -0700

    Add files via upload
    Assignment page 63
    
    
    1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them 
       optional. Have the method do a math operation and return a integer result.
    
    2. In the Main() method of the console app, instantiate the class.
    
    3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second 
       number.
    
    4. Call the method in the class, passing in the one or two numbers entered.
    
    5. Try various combinations of numbers on the code, including having no second number.

---
+++ Assignment Page63/Assignment Page63/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Page63
+{
+    public class class2
+    {
+        public int multiply(int num1, int num2 = 5)
+        {
+            return num1 * num2;
+
+        }
+    }
+}
+
+
---
+++ Assignment Page63/Assignment Page63/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Page63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class2 operation2 = new class2();
+
+            Console.WriteLine("Please Enter A Number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Another Number, but you don't have to!:");
+            string num2 = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(num2))
+            {
+                Console.WriteLine(operation2.multiply(num1, num2: 5));
+                Console.ReadLine();
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                Console.WriteLine(operation2.multiply(num1, num3));
+            }
+            Console.ReadLine();
+        }
+    }
+}
