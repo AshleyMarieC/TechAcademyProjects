e6681782d47d4830505fea479b5751e126d33b0a
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-02 13:33:44 -0700

    Add files via upload

---
+++ Assignment pg76/Assignment pg76/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg76
+{
+        public class Employee : Person, IQuittable
+        {
+            public override void SayName()
+            {
+                //throw new NotImplementedException();
+                Console.WriteLine(FirstName + " " + LastName);
+            }
+
+            public void Quit()
+            {
+            Console.WriteLine("I quit!");
+            }
+
+        }
+    
+}
