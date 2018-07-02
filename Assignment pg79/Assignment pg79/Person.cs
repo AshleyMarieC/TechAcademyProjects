720b488e67b12206a3613fd4f470f0917e4780f7
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-02 15:37:50 -0700

    Add files via upload

---
+++ Assignment pg79/Assignment pg79/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg79
+{
+    class Person
+    {
+        public string Employee;
+        public int ID;
+        public Person(string Employee, int ID)
+        {
+            this.Employee = Employee;
+            this.ID = ID;
+        }
+        public static bool operator ==(Person person1, Person person2)
+        {
+            if (person1.Employee == person2.Employee &&
+             person1.ID == person2.ID)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+
+        public override bool Equals(object obj)
+        {
+           if (!(obj is Person))
+           {
+               return false;
+           }
+           return this == (Person)obj;
+        }
+    }
+}
---
+++ Assignment pg79/Assignment pg79/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person("Ashley", 15632);
+            Person person2 = new Person("Jackie", 15645);
+
+            if (person1.ID == person2.ID)
+                Console.WriteLine("ID one DOES equal ID two");
+            else
+                Console.WriteLine("ID one DOES NOT equal ID two");
+            Console.ReadLine();
+
+        }
+        
+    }
+}
+
