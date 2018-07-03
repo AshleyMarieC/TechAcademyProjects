d0b093bf452b5697e7fac7f92dd40a29d888335d
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-02 20:22:05 -0700

    Add files via upload

---
+++ Assignment pg81/Assignment pg81/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg81
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+    }
+}
---
+++ Assignment pg81/Assignment pg81/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            {
+                employee.Things.Add("desk");
+                employee.Things.Add("monitor");
+                employee.Things.Add("keyboard");
+            }
+
+            Employee<int> employee2 = new Employee<int>();
+            {
+                employee2.Things.Add(2);
+                employee2.Things.Add(10);
+                employee2.Things.Add(15);
+
+            }
+
+
+        }
+    }
+}
