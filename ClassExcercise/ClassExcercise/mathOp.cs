9731ef6394348a9d72b34d466ed7d862e459da55
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-06 20:41:01 -0700

    Update mathOp.cs

--- ClassExcercise/ClassExcercise/mathOp.cs
+++ ClassExcercise/ClassExcercise/mathOp.cs
@@ -8,25 +8,28 @@ namespace ClassExcercise
 {
     public class mathOp
     {
-        public int Square(int num1, int num2)
+        public int classExercise(int num1, int num2)
         {
             return num1 * num2;
         }
-        public decimal Add(decimal num1)
+        public decimal classExercise(decimal num1)
         {
-            return num1 + num1;
+            return Convert.ToInt32(num1 + num1);
+
         }
-        public string length(string string1)
+        public int classExercise(string string1)
         {
-            return Convert.ToString(string1.Length);
-        }
-    }
-    
-    public class class2
-    {
-        public int multiply(int num1, int num2= 5)
-        {
-            return num1 * num2;
+            try
+            {
+                int int1 = Convert.ToInt32(string1);
+                return int1 + int1;
+            }
+            catch
+            {
+                Console.WriteLine("I'm sorry, but that conversion can not take place");
+                return 0;
+            }
         }
     }
 }
+
