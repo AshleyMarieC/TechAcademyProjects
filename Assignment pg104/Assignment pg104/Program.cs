1df6f876db4bc2c8c02fe4a48a5e1eaeeaf53b65
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-09 15:32:06 -0700

    Add files via upload

---
+++ Assignment pg104/Assignment pg104/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_pg104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Please enter a number: ");
+            string number = string.Format(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Ashley\source\repos\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+
+            //string number = Console.ReadLine();
+            //File.WriteAllText(@"C:\Users\Ashley\source\repos\log.txt", number);
+
+           string printNumber = File.ReadAllText(@"C:\Users\Ashley\source\repos\log.txt");
+           Console.WriteLine(printNumber);
+           Console.ReadLine();
+        }
+    }
+}
