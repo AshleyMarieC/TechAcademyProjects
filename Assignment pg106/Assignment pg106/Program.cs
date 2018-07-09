e7a840389d1157014675cbf3ebe5bcf18dfc5377
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-09 15:58:08 -0700

    Add files via upload

---
+++ Assignment pg106/Assignment pg106/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine("It will be {0} in {1} hours from now.",futureTime,hours);
+
+            Console.ReadLine();
+        }
+    }
+}
