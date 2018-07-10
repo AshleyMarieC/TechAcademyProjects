1afbdd51040df5fe9b9c3b7700181c3640c553c4
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-10 14:24:50 -0700

    Add files via upload

---
+++ Assignment pg116/Assignment pg116/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment_pg116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Hello, How old are you?");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                var todaysDate = DateTime.Today.Year;
+                var birthYear = todaysDate - age;
+                Console.WriteLine("It looks like you were born in {0}", birthYear);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("I'm sorry, you can not enter 0 or a negative number");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: Please enter your age using numbers only");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
