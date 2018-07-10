5e7206398a8a70c64d87851d0ed2e3d3ba556cbe
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-07-10 12:33:09 -0700

    Add files via upload

---
+++ Assignment pg 113/Assignment pg 113/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Ashley";
+            var myAge = 31;
+
+            var person = new Person(myName, myAge);
+            Console.WriteLine("Hello, my name is {0} and I am {1} years old", myName, myAge);
+
+            var chainedPerson = new Person(myName);
+            Console.WriteLine("Hello, my name is {0} and I am {1} years old",myName, person.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
