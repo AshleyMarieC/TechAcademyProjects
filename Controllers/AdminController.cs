4149d11d7caa5d1ad76ebe1676cbe506864f0ead
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-08-03 20:07:58 -0700

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Insurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var signups = db.Quotes;
+                var signupVms = new List<SignUp>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignUp();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVm.TotalQuote = Convert.ToDecimal(signup.TotalQuote);
+                 
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Insurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Quote(string FirstName, string LastName, string EmailAddress, string DateOfBirth, int CarYear, string CarMake,
+            string CarModel, bool DUI, int Tickets, string Coverage, decimal TotalQuote)
+        {
+                using (InsuranceEntities db = new InsuranceEntities())
+                {
+                var user = new Quote();
+                user.FirstName = FirstName;
+                user.LastName = LastName;
+                user.EmailAddress = EmailAddress;
+                user.DateOfBirth = DateOfBirth;
+                user.CarYear = CarYear;
+                user.CarMake = CarMake;
+                user.CarModel = CarModel;
+                user.DUI = Convert.ToString(DUI);
+                user.Tickets = Tickets;
+                user.Coverage = Coverage;
+                user.TotalQuote = Convert.ToInt32(TotalQuote);
+                    
+                db.Quotes.Add(user);
+                db.SaveChanges();
+
+                }
+                return View("Success");
+        }
+
+    }
+}
