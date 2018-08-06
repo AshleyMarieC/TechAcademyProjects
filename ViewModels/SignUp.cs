09554224261b8c75f8334c6dd4c3767c4fcb9985
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-08-06 16:48:48 -0700

    Update SignUp.cs

--- ViewModels/SignUp.cs
+++ ViewModels/SignUp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +11,13 @@ namespace Insurance.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        //public string DateOfBirth { get; set; }
-        //public int CarYear { get; set; }
-        //public string CarMake { get; set; }
-        //public string CarModel { get; set; }
-        //public bool Dui { get; set; }
-        //public int SpeedingTickets { get; set; }
-        //public string Coverage { get; set; }
+        public string DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string Dui { get; set; }
+        public int SpeedingTickets { get; set; }
+        public string Coverage { get; set; }
         public decimal TotalQuote { get; set; }
     }
 }
