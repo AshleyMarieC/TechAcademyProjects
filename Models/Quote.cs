f1bd95d4283133375b03b6cb8d8d1e8291c861fd
AshleyMarieC <39418831+AshleyMarieC@users.noreply.github.com>
2018-08-06 16:49:39 -0700

    Update Quote.cs

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -18,13 +18,13 @@ namespace Insurance.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public Nullable<System.DateTime> DateOfBirth { get; set; }
-        public Nullable<int> CarYear { get; set; }
+        public string DateOfBirth { get; set; }
+        public int CarYear { get; set; }
         public string CarMake { get; set; }
         public string CarModel { get; set; }
         public string DUI { get; set; }
         public string Coverage { get; set; }
-        public Nullable<int> Tickets { get; set; }
-        public Nullable<int> TotalQuote { get; set; }
+        public int Tickets { get; set; }
+        public decimal TotalQuote { get; set; }
     }
 }
